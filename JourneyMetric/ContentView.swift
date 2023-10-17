9ccc537c145ac047ff13eeba5d91021595416760
Jacek <jacek.dogiel@gmail.com>
2023-10-17 10:39:32 +0200

    Initial Commit

---
+++ JourneyMetric/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JourneyMetric
+//
+//  Created by Jacek Dogiel on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JourneyMetric/JourneyMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyMetricApp.swift
+//  JourneyMetric
+//
+//  Created by Jacek Dogiel on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct JourneyMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
